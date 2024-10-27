352bfc9a1d8b1304f44feffc830b62c56651d5e7
LeticiaBianca <leticiabiancaoliveira@gmail.com>
2024-10-27 12:56:03 -0300

    Corrigindo regularity

--- main.py
+++ main.py
@@ -667,10 +667,18 @@ def tamura_regularity(img):
     std_devs = np.array([np.std([coarseness]), 
                          np.std([contrast]), 
                          np.std([directionality]), 
-                         np.std([line_likeness])])
+                         np.std([line_likeness])], dtype='f')
     
-    # calc sum inverse of standard deviations
-    regularity = np.sum(1 / (std_devs + 1e-6))
+    # Calculate regularity with zero-checking per feature
+    regularity_contributions = []
+    for std_dev in std_devs:
+        if std_dev != 0:
+            regularity_contributions.append(1 / std_dev)
+        else:
+            regularity_contributions.append(0)
+    
+    # Sum the contributions to get the regularity score
+    regularity = np.sum(regularity_contributions)
 
     return regularity
 
