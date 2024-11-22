dd5607af042ea18c700b109c2ed823ce141541fe
inathlia <nathtenaglia@hotmail.com>
2024-11-22 20:40:34 -0300

    classify external image - calc HI attribute

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ roi_data = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
             '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
             '50', '51', '52', '53', '54']
 
-# processed_data = {} # flatten images
 class_data = {} # "saudavel" or "possui esteatose"
 caracteristic_data = {} # coordenates, HI, entropy...
 accuracy_results = []
@@ -56,11 +55,11 @@ specificities_results = []
 aggregated_cm = [[0, 0], [0, 0]]
 models = []
 
-class_mapping = {
-                    "possui esteatose": 0,
-                    "saudável": 1
-                }
+class_mapping = {"possui esteatose": 0, "saudável": 1}
 inverse_class_mapping = {v: k for k, v in class_mapping.items()}
+image_to_classify = np.array([0.0, 0.0, 0.0, 0.0, 0.0,
+                              0.0, 0.0, 0.0, 0.0, 0.0,
+                              0.0, 0.0, 0.0]) # same as caracteristics
 
 # ----------------------------- FUNCTIONALITIES (Part 1) ----------------------------
 def update_excel(file_name, column, value):
@@ -87,7 +86,32 @@ def update_excel(file_name, column, value):
         print(f"Valor atualizado na célula correspondente ao arquivo '{file_name}' e coluna '{column}'.")
     except Exception as e:
         print(f"Erro ao salvar o arquivo Excel: {idx}")
-        
+
+def update_csv(file_path, column, value):
+    # Load the CSV file into a DataFrame
+    file_name = "rois_informations.csv"
+    file_path = os.path.join(os.getcwd(), file_name)
+    
+    try:
+        df = pd.read_csv(file_path, delimiter=";")
+        print(df.columns)
+    except FileNotFoundError:
+        print(f"Arquivo '{file_name}' não foi encontrado.")
+        return
+
+    if column not in df.columns:
+        print(f"A coluna '{column}' não foi encontrada.")
+        return
+
+    idx = df[df.iloc[:, 0] == file_name].index[0]
+    \
+    try:
+        df.at[idx, column] = value
+
+        df.to_csv(file_path, index=False)
+        print(f"Valor atualizado na célula correspondente ao arquivo '{file_name}' e coluna '{column}'.")
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo CSV: {idx}")
 
 # plot images with histogram
 def display_image_and_histogram(image, title="Imagem", parent=None, isROI=False):
@@ -293,7 +317,7 @@ def save_image(roi_image):
         ultrasound += 1
 
 # calc index HI value
-def calc_HI(roi_kidney, roi_liver, coord_kidney, coord_liver):
+def calc_HI(roi_kidney, roi_liver, coord_kidney, coord_liver, classification):
     
     average_kidney = np.mean(roi_kidney)
     average_liver = np.mean(roi_liver)
@@ -320,17 +344,22 @@ def calc_HI(roi_kidney, roi_liver, coord_kidney, coord_liver):
     # values can't be greater than 255
     adjusted_roi_liver = np.clip(adjusted_roi_liver, 0, 255)
     
-    update_excel(f"ROI_{str(patient).zfill(2)}_{ultrasound}","Coordenadas Fígado", f"{coord_liver[0]}, {coord_liver[1]}")
-    update_excel(f"ROI_{str(patient).zfill(2)}_{ultrasound}", "Coordenadas Cortex Renal", f"{coord_kidney[0]}, {coord_kidney[1]}")
-    update_excel(f"ROI_{str(patient).zfill(2)}_{ultrasound}", "HI",hi_ratio)
+    if classification == 0:
+        update_csv(f"ROI_{str(patient).zfill(2)}_{ultrasound}","Coordenadas Figado", f"{coord_liver[0]}, {coord_liver[1]}")
+        update_csv(f"ROI_{str(patient).zfill(2)}_{ultrasound}", "Coordenadas Cortex Renal", f"{coord_kidney[0]}, {coord_kidney[1]}")
+        update_csv(f"ROI_{str(patient).zfill(2)}_{ultrasound}", "HI",hi_ratio)
+        
+        save_image(adjusted_roi_liver)
+    else:
+        global image_to_classify
+        image_to_classify = np.insert(image_to_classify, 0, hi_ratio)
+        print(image_to_classify)
     
-    save_image(adjusted_roi_liver)
-
     show_hi_ratio_window(hi_ratio)
     
 
 # ROI -> Region of Interest
-def cut_rois(image):
+def cut_rois(image, classification=0):
     image_copy = image.copy()
     click_count = [0]
 
@@ -353,7 +382,6 @@ def cut_rois(image):
     ax.set_title("Clique para selecionar a ROI do Figado")
     ax.imshow(cv2.cvtColor(image_copy, cv2.COLOR_BGR2RGB))
     canvas.draw()
-
     def click_event(event):
         nonlocal roi_kidney, roi_liver, coord_liver, coord_kidney
 
@@ -384,7 +412,7 @@ def cut_rois(image):
                     cv2.rectangle(image_copy, (x - 14, y - 14),
                                   (x + 14, y + 14), (0, 255, 0), 2)
 
-                    calc_HI(roi_rim, roi_liver, coord_rim, coord_liver)
+                    calc_HI(roi_rim, roi_liver, coord_rim, coord_liver, classification)
 
                 # update image with roi draw
                 ax.clear()
@@ -405,9 +433,12 @@ def select_rois():
     image_path = filedialog.askopenfilename(
         filetypes=[("Imagens e MAT", "*.png *.jpg *.mat")]
     )
-
+    
     if image_path:
-        if image_path.lower().endswith(('*.png', '*.jpg', '*.jpeg')):
+        png = image_path.lower().endswith('.png')
+        jpg = image_path.lower().endswith('.jpg')
+        jpeg = image_path.lower().endswith('.jpeg')
+        if png or jpg or jpeg:
             cut_rois(cv2.imread(image_path))
         elif image_path.lower().endswith('.mat'):
             mat = scipy.io.loadmat(image_path)
@@ -707,26 +738,6 @@ def on_closing():
 
 # --------------------------------- XGBOOST --------------------------------------
 
-# def process_images_to_dataframe(image_directory=""):
-#     for label, file_list in roi_data.items():
-#         processed_data[label] = []
-#         for file_name in file_list:
-#             file_path = os.path.join(image_directory, f"{file_name}.jpg")
-#             if os.path.exists(file_path):
-#                 # convert image to matrix
-#                 image = Image.open(file_path)
-#                 image_array = np.array(image).flatten().tolist()  # flatten and convert to list
-#                 processed_data[label].append(image_array)
-#             else:
-#                 print(f"Imagem não encontrada: {file_path}")
-#     print_dataframe()
-
-# def print_dataframe(): 
-#     for label, images in processed_data.items():
-#         print(f"Paciente: {label}")
-#         for idx, image_array in enumerate(images):
-#             print(f"  Imagem {idx}: {image_array[:10]}... (total de {len(image_array)} pixels)")
-
 def load_roi_csv_info(csv_path="rois_informations.csv"):
     global class_data
     global caracteristic_data
@@ -759,7 +770,7 @@ def load_roi_csv_info(csv_path="rois_informations.csv"):
         row["Nome do arquivo"]: row[caracteristic_colums].values
         for _, row in df.iterrows()
     } 
-    # print(class_data)
+    # print(caracteristic_data)
 
 def xgboost():
     global aggregated_cm
@@ -852,15 +863,33 @@ def show_confusion_matrix_all_patients():
 
 # AJUSTAR PARA QUALQUER IMAGEM
 def xgboost_classification(csv_path="rois_informations.csv"):
-    # ler a imagem
-    image = 'ROI_00_0'
+    load_roi_csv_info()
 
-    # extrair classificadores da imagem (HI, entropia, homogeneidade, ...)
+    # check if theirs trained models
+    if not models:
+        xgboost()
+      
+    # read image
+    image_path = filedialog.askopenfilename(filetypes=[("Imagens", "*.png *.jpg")])
     df = pd.read_csv(csv_path, delimiter=";")
-    image_row = df[df['Nome do arquivo'] == image]
-    image_data = image_row.iloc[:, 4:]
-    image_data = image_data.replace(",", ".", regex=True).astype(float)
-    image_data = image_data.apply(pd.to_numeric)
+    column = 'Nome do arquivo'
+
+    if image_path:
+        start = image_path.rfind("/") + 1  # Add 1 to exclude the '/'
+        end = image_path.rfind(".")
+        image = image_path[start:end]        # Don't include the '.'
+        if image in df[column].values:
+            # extrair classificadores da imagem (HI, entropia, homogeneidade, ...)
+            image_row = df[df['Nome do arquivo'] == image]
+            image_data = image_row.iloc[:, 4:]
+            image_data = image_data.replace(",", ".", regex=True).astype(float)
+            image_data = image_data.apply(pd.to_numeric)
+        else:
+            image_data = classify_new_image(image_path)
+            # messagebox.showerror("Erro", f"Coloque uma imagem presente no csv.")
+            print("acabou!")
+            return
+
 
     # classificar a imagem
     preds = [model.predict(image_data) for model in models]
@@ -868,6 +897,11 @@ def xgboost_classification(csv_path="rois_informations.csv"):
     avg_preds = np.mean(preds, axis=0)
     class_names = [inverse_class_mapping[pred] for pred in avg_preds]
     print(class_names)
+
+def classify_new_image(image_path):
+    cut_rois(cv2.imread(image_path), 1)
+
+    # return data
 
 # ------------------------------------ GUI ------------------------------------------
 
@@ -936,21 +970,9 @@ go_to_image_button.grid(row=0, column=10, padx=5)  # go to index
 button_frame_class = Frame(root)
 button_frame_class.pack(pady=10)
 
-# convert_roi_1d_array = Button(
-#     button_frame_class, text='Converter ROIs', command=process_images_to_dataframe)
-# convert_roi_1d_array.grid(row=1, column=0, padx=5)
-
-load_roi_csv = Button(
-    button_frame_class, text='Extrair dados das ROIs (csv)', command=load_roi_csv_info)
-load_roi_csv.grid(row=1, column=0, padx=5)
-
-run_xgboost = Button(
-    button_frame_class, text='XGBoost', command=xgboost)
-run_xgboost.grid(row=1, column=1, padx=5)
-
 classify_xgboost = Button(
     button_frame_class, text='Classificar Imagem (XGBoost)', command=xgboost_classification)
-classify_xgboost.grid(row=1, column=2, padx=5)
+classify_xgboost.grid(row=1, column=0, padx=5)
 
 # Buttons Grid 2 ------------------------------------------------------------------ End
 
